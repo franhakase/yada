b15ba6aac0006a8ff9c9954f728ac384924f2609
David Moreira <d.a.v.i.d.s.m@hotmail.com>
2021-04-03 11:55:21 -0300

    - Mudanças no comportamento do monitor de clipboard; - Mudanças no comportamento do diálogo de tradução.

--- app/src/main/java/eu/franhakase/yada/NewDialogTranslationActivity.java
+++ app/src/main/java/eu/franhakase/yada/NewDialogTranslationActivity.java
@@ -15,31 +15,36 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.core.content.ContextCompat;
 import java.util.Locale;
 import java.util.Objects;
 import eu.franhakase.yada.kuromoji.KanaToRomaji;
 import eu.franhakase.yada.kuromoji.Kuromoji;
 
-public class NewDialogTranslationActivity extends Activity implements Button.OnClickListener
+public class NewDialogTranslationActivity extends Activity implements Button.OnClickListener, ClipboardManager.OnPrimaryClipChangedListener
 {
-    KanaToRomaji kn;
-    String Translit = "";
-    String Original = "";
+    private KanaToRomaji kn;
+    private String Original = "";
     boolean bResume = false;
+    boolean bFirstFinish = false;
+    private WebView wvMain;
+    private ClipboardManager clipboard;
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_translation_dialog);
-        this.setFinishOnTouchOutside(true);
         kn = new KanaToRomaji();
         LinearLayout ll = findViewById(R.id.dlMain);
         ll.setBackground(ContextCompat.getDrawable(this, R.drawable.ic_rounded_rectangle));
@@ -50,6 +55,24 @@ public class NewDialogTranslationActivity extends Activity implements Button.OnC
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
         Button btTransliterate = findViewById(R.id.btTransliterate);
         btTransliterate.setOnClickListener(this);
+        wvMain = findViewById(R.id.wvMain);
+        wvMain.getSettings().setJavaScriptEnabled(true);
+        wvMain.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url)
+            {
+                super.onPageFinished(view, url);
+                if(!bFirstFinish)
+                {
+                    bFirstFinish = true;
+                    if(url.toLowerCase().contains("deepl.com"))
+                    {
+                        wvMain.loadUrl(getResources().getString(R.string.app_deepl_webpage_cleanup));
+                    }
+                }
+            }
+        });
+
     }
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -59,18 +82,15 @@ public class NewDialogTranslationActivity extends Activity implements Button.OnC
         if(hasFocus && !bResume)
         {
             bResume = true;
-            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            clipboard.addPrimaryClipChangedListener(this);
             ClipData clip = clipboard.getPrimaryClip();
             if(   clip != null && clip.getItemCount() > 0 && clip.getItemAt(0).getText() != null)
             {
                 Original = clip.getItemAt(0).getText().toString();
                 String url = String.format(Locale.getDefault(), "https://www.deepl.com/translator#auto/en/%s", Uri.encode(clip.getItemAt(0).getText().toString()));
-                //Translit = Kuromoji.TokenizeKaraoke(Original);//clip.getItemAt(0).getText().toString() + "\r\n\r\n-\r\n\r\n" + kn.convert(Kuromoji.Tokenize(clip.getItemAt(0).getText().toString()));
-                WebView wvMain = findViewById(R.id.wvMain);
-                wvMain.getSettings().setJavaScriptEnabled(true);
                 wvMain.loadUrl(Objects.requireNonNull(url));
             }
-
         }
         super.onWindowFocusChanged(hasFocus);
     }
@@ -81,7 +101,6 @@ public class NewDialogTranslationActivity extends Activity implements Button.OnC
         {
             return String.format("%s\r\n%s", Original, kn.convert(this, Kuromoji.Tokenize(Original)));
         }
-
         else if (mode == 1)
         {
             return Kuromoji.TokenizeKaraoke(this, Original);
@@ -94,7 +113,8 @@ public class NewDialogTranslationActivity extends Activity implements Button.OnC
         LayoutInflater inflater= LayoutInflater.from(this);
         View view = inflater.inflate(R.layout.romaji_dialog_layout, null);
         final TextView textview = view.findViewById(R.id.txtResult);
-        textview.setText(Translit);
+        String translit = "";
+        textview.setText(translit);
         textview.setMovementMethod(new ScrollingMovementMethod());
         Spinner spnMode = view.findViewById(R.id.spnRomajiMode);
         ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.lst_romaji_mode));
@@ -133,6 +153,25 @@ public class NewDialogTranslationActivity extends Activity implements Button.OnC
         if(view.getId() == R.id.btTransliterate)
         {
             mostrarRomanizado();
+        }
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        clipboard.removePrimaryClipChangedListener(this);
+        //Toast.makeText(this, "safe", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPrimaryClipChanged()
+    {
+        ClipData clip = clipboard.getPrimaryClip();
+        if(clip != null && clip.getItemCount() > 0 && clip.getItemAt(0).getText() != null)
+        {
+            Original = clip.getItemAt(0).getText().toString();
+            Toast.makeText(this, "Novo conteúdo pronto para transliteração!", Toast.LENGTH_SHORT).show();
         }
     }
 }
